53435edb43f3baa69103f6f0bf693872ee4f4c24
CatherineWong1 <littlerainywq@163.com>
2019-07-04 22:16:41 +0800

    Update preprocess.py

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -5,7 +5,7 @@ Date: 20190704
 实现的功能：
 1. 对src.txt和tgt.txt进行预处理，得到处理好的数据集，将数据集处理成Json的格式，但保存为.pt文件。
 由于需要考虑在训练时，选一个batch_size的数据，因此数据集格式如下：
-[{ "data":
+[{ "segment":
   [{"src":原文段落1, "tgt":标题1, src_tokens:原文对应的wordPiece id, src_cls:原文对应的句子分隔, tgt_tokens:标题对应的wordPiece id},
    {"src":原文段落1.1, "tgt":标题1.1, src_tokens:原文对应的wordPiece id, src_cls:原文对应的句子分隔, tgt_tokens:标题对应的wordPiece id}]
 }
@@ -13,9 +13,7 @@ Date: 20190704
 
 """
 
-import re
 import nltk
-import numpy as np
 import torch
 import gc
 from pytorch_pretrained_bert import BertTokenizer
@@ -75,29 +73,97 @@ class BertData():
         return src_tokens, segment_ids, tgt_tokens
 
 
-def format_to_bert_multi(dataset_lists, save_file):
+def format_to_bert(raw_lists, save_file):
     """
-    转换raw text为需存储的格式
-    :param dataset_lists:
-    :param save_file:
+    将raw_lists转换成所需的数据格式
+    :param raw_lists: 整合src.txt和tgt.txt后的数据列表
+    :param save_file: 需要保存的数据集名称
     :return:
     """
     bert_data = BertData()
-    datasets = []
+    vocabulary = []
 
-    for i in range(len(dataset_lists)):
-        data_dict = dict()
-        src = dataset_lists[i]
-        if len(src) == 0:
-            return None
+    for i in range(len(raw_lists)):
+        item_list = raw_lists[i]['segment']
+        for item in item_list:
+            src = item['src_txt']
+            tgt = item['tgt_txt']
+            if bert_data.preprocess(src,tgt) is not None:
+                src_tokens, segment_ids, tgt_tokens = bert_data.preprocess(src, tgt)
+                item['src'] = src_tokens
+                item['segs'] = segment_ids
+                item['tgt'] = tgt_tokens
 
-        if bert_data.preprocess_multi(src) is not None:
-            src_tokens, segment_ids, cls_ids = bert_data.preprocess_multi(src)
+            # 调用create vocabulary
+            vocabulary = create_vocalbulary(src,tgt,vocabulary)
 
-            b_data_dict = {'src': src_tokens, 'segs':segment_ids,'clss': cls_ids,
-                           'src_txt': src}
-            datasets.append(b_data_dict)
+    torch.save(raw_lists,save_file)
 
-    torch.save(datasets,save_file)
-    datasets = []
+    vocab_file = "vocab"
+    torch.save(vocabulary, vocab_file)
+
     gc.collect()
+
+
+def load_data(src_file,tgt_file):
+    """
+    将raw_txt整合成后续可处理的列表嵌套字典的格式
+    :param src_file: src.txt
+    :param tgt_file: tgt.txt
+    :return: raw_lists
+    """
+    raw_lists = []
+    item_lists = []
+    item_flag = 0
+    tgt_f = open(tgt_file, 'r', errors='ignore')
+
+    with open(src_file,'r',errors='ignore') as src_f:
+        for line in src_f.readlines():
+            if line.count('\n') == len(line):
+                if item_flag == 0:
+                    item_flag = 1
+                    item_dict = {"segment": item_lists}
+                    raw_lists.append(item_dict)
+                    continue
+                else:
+                    continue
+            else:
+                item_flag = 0
+                raw_src = line.strip()
+                try:
+                    raw_tgt = next(tgt_f)
+                    raw_tgt = raw_tgt.strip()
+                    raw_dict = {"src_txt":raw_src, "tgt_txt":raw_tgt}
+                    item_lists.append(raw_dict)
+                except Exception as e:
+                    break
+
+    return raw_lists
+
+
+def create_vocalbulary(src,tgt,vocb):
+    """
+    生成不重复的词汇表
+    :param src: src文本
+    :param tgt: tgt文本
+    :param vocb: vocabulary list
+    :return: 新覆盖的 vocb
+    """
+    sent_lists = nltk.sent_tokenize(src)
+    for sent in sent_lists:
+        word_lists = nltk.word_tokenize(sent)
+        for word in word_lists:
+            try:
+                index = vocb.index(word)
+            except Exception as e:
+                vocb.append(word)
+
+    tgt_word = nltk.word_tokenize(tgt)
+    for t_word in tgt_word:
+        try:
+            index = vocb.index(t_word)
+        except Exception as e:
+            vocb.append(t_word)
+
+    return vocb
+
